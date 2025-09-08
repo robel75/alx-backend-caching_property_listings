7bdbab7e964631b176c97ede7f3207e0446f57ae
Robel <robelisgone2@gmail.com>
2025-09-08 19:44:27 +0300

    up

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
+from django.http import JsonResponse
+from .models import Property
+@cache_page(60 * 15)
+def property_list(request):
+    properties = Property.objects.all().values(
+        'id', 'title', 'description', 'price', 'location', 'created_at'
+    )
+    return JsonResponse(list(properties), safe=False)
 
-# Create your views here.
